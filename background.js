f656201fd8f20830e113f6f43f610c9a31e34330
toufudealer <ata.fb37@gmail.com>
2025-07-26 16:26:42 +0300

    .

--- background.js
+++ background.js
@@ -5,19 +5,12 @@ window.addEventListener('DOMContentLoaded', () => {
     './resources/backgrounds/aigis.jpg',
     './resources/backgrounds/yes.jpg',
     './resources/backgrounds/ambatukam.jpg',
-    './resources/backgrounds/cem.jpg'
+    './resources/backgrounds/cem.jpg',
   ];
 
   const randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   const bgDiv = document.querySelector('.background-container');
   if (bgDiv) {
-    const img = new Image();
-    img.src = randomBg;
-    img.onload = () => {
-      bgDiv.style.backgroundImage = `url('${randomBg}')`;
-    };
-    img.onerror = () => {
-      bgDiv.style.background = "#222"; // Yedek arka plan rengi
-    };
+    bgDiv.style.backgroundImage = `url('${randomBg}')`;
   }
 });
