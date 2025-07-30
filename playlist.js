eef457d702b64f3e3cb17d1103471b40ebce966f
toufudealer <ata.fb37@gmail.com>
2025-07-31 00:57:24 +0300

    .

---
+++ playlist.js
@@ -0,0 +1,8 @@
+// Müzik Listesi
+// Şarkı adlarını "name", linklerini "src" olarak ekle.
+const musicPlaylist = [
+  { name: "Shihoko Hirata - Heaven", src: "https://files.catbox.moe/jzs49b.mp3" },
+  { name: "Lyn - No More What Ifs", src: "https://files.catbox.moe/d3law2.mp3" },
+  { name: "AKFG - Social Withdrawal", src: "https://files.catbox.moe/r59f5o.mp3" }
+  // Kendi müziklerini bu formatta ekleyebilirsin.
+];
